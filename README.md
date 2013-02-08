616836d72f7b27c099d972dc0150132646a01035
João Carlos <joaocarlos422@gmail.com>
2013-02-08 11:59:27 -0200

    improving golem

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-golem-py
-========
+golem
+=====
+
+golem - file-copy and backup tool
+
+An animated anthropomorphic being, created entirely from inanimate matter.
+
+Usage
+-----
+
+	python golem.py <source> [<dest>]
+
+config.json
+-----------
+
+Put a **config.json** inside your project to tell something to **golem**.
+
+###Ignored
+Ignore specified files and/or folders.
+
+Exemple
+
+	{
+	    "Ignored": ["file1", "file2", "folder/file3"]
+	}
+
+With this configuration, golem not will copy and backup **file1**, **file2** and **file3** in **folder**
+
+###Destination
+Define the file-copy and backup destination. Params **dest** is optional if **Destination** is set in **config.json**
+
+Exemple
+
+	{
+	    "Destination": "path/to/a/folder"
+	}
--- golem.py
+++ golem.py
@@ -1,28 +1,51 @@
+import json
 import os
 import sys
-import zipfile
 import shutil
 import time
-import json
+import zipfile
 
 source = sys.argv[1]
-dest = sys.argv[2]
+zipname = time.strftime("%Y%m%d-%H%M%S") + ".zip"
 config = os.path.join(source,"config.json")
-zipname = time.strftime("%Y%m%d - %H %M %S") + ".zip"
 
 if os.path.exists(config):
-  f = open(config)
-	ignore = json.load(f)['Ignore']
-	f.close()
+	f = open(config)
+	f = json.load(f)
+	if 'Destination' in f.keys():
+		dest = f['Destination']
+	else:
+		dest = sys.argv[2]
+else:
+	dest = sys.argv[2]
 
-if os.path.exists(dest):
+def ignoreList(suffix): #monting list of ignored files
+	ignore = [config]
+	if os.path.exists(config):
+		f = open(config)
+		for i in json.load(f)['Ignore']:
+			ignore.append(os.path.join(suffix,i))
+		f.close()
+	return ignore
+
+ignore = ignoreList(dest)
+paths = []
+if os.path.exists(dest): #zipping destination files
 	zip = zipfile.ZipFile(zipname, "w")
 	for root, dirs, files in os.walk(dest):
 		if root not in ignore:
 			for file in files:
-				if file not in ignore:
+				if os.path.join(dest,file) not in ignore:
 					zip.write(os.path.join(root, file))
 
-	shutil.rmtree(dest)
+	zip.close()
+	shutil.rmtree(dest) #removing destination files
 
-shutil.copytree(source, dest)
+ignore = ignoreList(source)
+if os.path.exists(source): #copying source files to destination
+	for root, dirs, files in os.walk(source):
+		if root not in ignore:
+			os.mkdir(root.replace(source,dest + os.sep))
+			for file in files:
+				if os.path.join(source,file) not in ignore:
+					shutil.copy(os.path.join(source,file),os.path.join(dest,file))
